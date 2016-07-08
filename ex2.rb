bf370bb7bea49c90bc31a02d2e8b75e900937233
Cess <cessmbuguar@gmail.com>
2016-07-08 09:45:22 +0300

    comments in ruby

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# comment, this is so you can read your program lat#er.
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and the comment after is ignored
+   #
+   # # You can also use a comment to "disable" or :comment out a piece of code:
+  puts "This won't run."
+   #
+  puts "This will run."
