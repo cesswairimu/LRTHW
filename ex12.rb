a516e7849657f33ad67c4967dfdf910dbb43c049
Cess <cessmbuguar@gmail.com>
2016-07-08 12:48:35 +0300

    performing operations on users inputs

---
+++ ex12.rb
@@ -0,0 +1,12 @@
+print "Give me a number "
+number = gets.chomp.to_i
+
+bigger_number = number * 100
+puts "A bigger number is #{bigger_number}"
+
+print "Give me another number "
+number1 = gets.chomp.to_i
+
+small = number1 / 100
+puts "Small number is #{small}"
+
