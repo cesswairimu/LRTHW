4895d791c96c2ee379f062657d4955eab002f520
Cess <cessmbuguar@gmail.com>
2016-07-08 13:12:13 +0300

    using avgr

--- ex13.rb
+++ ex13.rb
@@ -2,3 +2,10 @@ me, you = ARGV
 
 puts "I know #{me}"
 puts "But the ghost is #{you}"
+
+ryan, lawr,terr, fella = $stdin.gets.chomp
+
+puts "You are #{ryan}"
+puts "I really miss #{lawr}"
+puts "OOOH u cutest #{terr}"
+puts "Hope you had a HDB #{fella}"
