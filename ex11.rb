1b7fdd1329e681c4fd540f734bc173e2034fb2d6
Cess <cessmbuguar@gmail.com>
2016-07-08 12:42:44 +0300

    prompting input in ruby

---
+++ ex11.rb
@@ -0,0 +1,11 @@
+print "What is your name "
+name = gets.chomp
+print "Tell me your age bunch "
+age = gets.chomp
+print "What do u love most bunch? "
+love = gets.chomp
+
+
+puts  "name: #{name}"
+puts "age: #{age}"
+puts "loves: #{love}"
